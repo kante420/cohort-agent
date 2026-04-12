d0b485a6e1f89e9ac549e7e95421505c53583951
kante420 <hugocantserra@gmail.com>
2026-04-12 14:44:54 +0200

    No test

--- test_db.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-from config import DB_PATH
-
-con = duckdb.connect(DB_PATH, read_only=True)
-df = con.execute("SELECT * FROM cohorte_medicaciones LIMIT 1").fetchdf()
-print(df.columns.tolist())
-con.close()
